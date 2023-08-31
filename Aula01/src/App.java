1ebd627ea56a8ec3a96e6109c9cfe36e07553e52
BrunaCardoso7 <obitadrawing@gmail.com>
2023-08-31 15:25:35 -0300

    treinando herança

--- Aula01/src/App.java
+++ Aula01/src/App.java
@@ -1,5 +1,27 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+     public class Cachorro{
+        String nome;
+        int idade;
+        int peso;
+        void infDog(){
+            System.out.println("A idade do Cachorro é: " + idade);
+            System.out.println("O peso do Cachorro é: " + peso);
+        }
+        void latir(){
+            System.out.println(nome + " disse: auau!");
+        }
+    }
+    public class Poodle extends Cachorro{
+        int quantPatas;
+    }
+    public static void main(String[] args) {
+        App app = new App();
+        Poodle Ray = app.new Poodle();
+        Ray.idade = 8;
+        Ray.peso = 10;
+        Ray.nome = "Ray";
+        Ray.quantPatas = 4;
+        Ray.infDog();
+        Ray.latir();
     }
 }
